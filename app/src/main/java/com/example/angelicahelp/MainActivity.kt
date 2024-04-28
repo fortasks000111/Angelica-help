faf2a382e92873d7eb87353e51d8aae983f0dce5
fortasks000111 <167869575+fortasks000111@users.noreply.github.com>
2024-04-28 21:15:53 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/angelicahelp/MainActivity.kt
+++ app/src/main/java/com/example/angelicahelp/MainActivity.kt
@@ -1,18 +1,19 @@
-package com.example.angelicahelp
+package com.example.angelicahelp  // объявление пакета кода, в котором хранится ui-тест
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.angelicahelp.ui.mainscreen.MainScreen
+import androidx.appcompat.app.AppCompatActivity  // импрот класса AppCompatActivity
+import android.os.Bundle  // импорт класса Bundle
+import com.example.angelicahelp.ui.mainscreen.MainScreen  // импорт класса MainScreen из пакета ui.mainscreen
 
+// объявление класса MainActivity, который наследует AppCompatActivity
 class MainActivity : AppCompatActivity() {
-
+    // переопределяем функцию onCreate, который принимает параметр savedInstanceState
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_screen)
+        super.onCreate(savedInstanceState)  // вызываем метод onCreate из родительского класса и передаем ему параметр savedInstanceState
+        setContentView(R.layout.activity_main_screen)  // указываем основной макет активного экрана
+        // проверяем: был ли savedInstanceState передан при создании активности
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainScreen())
-                .commitNow()
+            // заменяем R.id.container на MainScreen
+            supportFragmentManager.beginTransaction().replace(R.id.container, MainScreen()).commitNow()
         }
     }
 }
